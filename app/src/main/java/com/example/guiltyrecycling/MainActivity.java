6f2fe43858985cdefca9467c2141235a6bbbd288
Sarah Sollauer <sollauers@wit.edu>
2022-07-15 09:48:28 -0400

    page 1

--- app/src/main/java/com/example/guiltyrecycling/MainActivity.java
+++ app/src/main/java/com/example/guiltyrecycling/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.guiltyrecycling;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Test 5
+        ImageButton writeButton = (ImageButton)findViewById(R.id.writeButton);
+        ImageButton voiceButton = (ImageButton)findViewById(R.id.voiceButton);
+        ImageButton imageButton = (ImageButton)findViewById(R.id.imageButton);
+
+        writeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Log.v("myApp", "Write It button is clicked");
+                Intent intent = new Intent();
+                //writeActivity to be created by Luis
+                intent.setClass(MainActivity.this, WriteActivity.class);
+            }
+        });
+
+        voiceButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Log.v("myApp", "Speak It button is clicked");
+                Intent intent = new Intent();
+                //writeActivity to be created by Luis
+                intent.setClass(MainActivity.this, VoiceActivity.class);
+            }
+        });
+
+        imageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Log.v("myApp", "Visualize It button is clicked");
+                Intent intent = new Intent();
+                //writeActivity to be created by Luis
+                intent.setClass(MainActivity.this, ImageActivity.class);
+            }
+        });
     }
 }
