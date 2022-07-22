af7aeb5cd0fe225d66b50e4aaa77b8f71bebfab2
Luis V <47792109+vargasl4atwit@users.noreply.github.com>
2022-07-22 09:16:47 -0400

    Minor changes

---
+++ app/src/main/java/com/example/guiltyrecycling/GridItem.java
@@ -0,0 +1,8 @@
+package com.example.guiltyrecycling;
+
+import android.graphics.Bitmap;
+
+public class GridItem {
+    public Bitmap image;
+    public String title;
+}
--- app/src/main/java/com/example/guiltyrecycling/ImageActivity.java
+++ app/src/main/java/com/example/guiltyrecycling/ImageActivity.java
@@ -1,4 +1,65 @@
 package com.example.guiltyrecycling;
 
-public class ImageActivity {
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.GridView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class ImageActivity extends AppCompatActivity {
+    GridView gridView;
+    ArrayList<GridItem> grid = new ArrayList<GridItem>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.image_activity);
+
+        gridView = (GridView) findViewById(R.id.GridView01);
+
+        Bitmap glassIm, plasticIm, metalIm, paperIm, organicIm, ewasteIm;
+        glassIm = BitmapFactory.decodeResource(getResources(), R.drawable.glass);
+        plasticIm = BitmapFactory.decodeResource(getResources(), R.drawable.plastic);
+        metalIm = BitmapFactory.decodeResource(getResources(), R.drawable.metal);
+        paperIm = BitmapFactory.decodeResource(getResources(), R.drawable.paper);
+        organicIm = BitmapFactory.decodeResource(getResources(), R.drawable.organic);
+        ewasteIm = BitmapFactory.decodeResource(getResources(), R.drawable.ewaste);
+
+        GridItem glassItem = new GridItem();
+        glassItem.image = glassIm;
+        glassItem.title = "GLASS";
+        grid.add(glassItem);
+
+        GridItem plasticItem = new GridItem();
+        plasticItem.image = plasticIm;
+        plasticItem.title = "PLASTIC";
+        grid.add(plasticItem);
+
+        GridItem metalItem = new GridItem();
+        metalItem.image = metalIm;
+        metalItem.title = "METAL";
+        grid.add(metalItem);
+
+        GridItem paperItem = new GridItem();
+        paperItem.image = paperIm;
+        paperItem.title = "PAPER";
+        grid.add(paperItem);
+
+        GridItem organicItem = new GridItem();
+        organicItem.image = organicIm;
+        organicItem.title = "ORGANIC";
+        grid.add(organicItem);
+
+        GridItem ewasteItem = new GridItem();
+        ewasteItem.image = ewasteIm;
+        ewasteItem.title = "E-WASTE";
+        grid.add(ewasteItem);
+
+        GridItemAdapter gridItemAdapter = new GridItemAdapter(this, R.layout.grid_item, grid);
+        gridView.setAdapter(gridItemAdapter);
+
+    }
 }
