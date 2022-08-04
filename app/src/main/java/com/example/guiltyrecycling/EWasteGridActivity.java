c25c6f5c4d45cc55a712dcb23b363a129d7de71c
Mireille <eliem@wit.edu>
2022-08-03 20:22:56 -0400

    search

--- app/src/main/java/com/example/guiltyrecycling/EWasteGridActivity.java
+++ app/src/main/java/com/example/guiltyrecycling/EWasteGridActivity.java
@@ -20,47 +20,47 @@ public class EWasteGridActivity extends AppCompatActivity {
 
         gridView6 = (GridView) findViewById(R.id.EWasteGrid);
 
-        Bitmap EWaste1Im, EWaste2Im, EWaste3Im, EWaste4Im, EWaste5Im, EWaste6Im, EWaste7Im, EWaste8Im;
-        EWaste1Im = BitmapFactory.decodeResource(getResources(), R.drawable.EWaste1);
-        EWaste2Im = BitmapFactory.decodeResource(getResources(), R.drawable.EWaste2);
-        EWaste3Im = BitmapFactory.decodeResource(getResources(), R.drawable.EWaste3);
-        EWaste4Im = BitmapFactory.decodeResource(getResources(), R.drawable.EWaste4);
-        EWaste5Im = BitmapFactory.decodeResource(getResources(), R.drawable.EWaste5);
-        EWaste6Im = BitmapFactory.decodeResource(getResources(), R.drawable.EWaste6);
-        EWaste7Im = BitmapFactory.decodeResource(getResources(), R.drawable.EWaste7);
-        EWaste8Im = BitmapFactory.decodeResource(getResources(), R.drawable.EWaste8);
-
-        GridItem EWaste1Item = new GridItem();
-        EWaste1Item.image = EWaste1Im;
-        grid6.add(EWaste1Item);
-
-        GridItem EWaste2Item = new GridItem();
-        EWaste2Item.image = EWaste2Im;
-        grid6.add(EWaste2Item);
-
-        GridItem EWaste3Item = new GridItem();
-        EWaste3Item.image = EWaste3Im;
-        grid6.add(EWaste3Item);
-
-        GridItem EWaste4Item = new GridItem();
-        EWaste4Item.image = EWaste4Im;
-        grid6.add(EWaste4Item);
-
-        GridItem EWaste5Item = new GridItem();
-        EWaste5Item.image = EWaste5Im;
-        grid6.add(EWaste5Item);
-
-        GridItem EWaste6Item = new GridItem();
-        EWaste6Item.image = EWaste6Im;
-        grid6.add(EWaste6Item);
-
-        GridItem EWaste7Item = new GridItem();
-        EWaste7Item.image = EWaste7Im;
-        grid6.add(EWaste7Item);
-
-        GridItem EWaste8Item = new GridItem();
-        EWaste8Item.image = EWaste8Im;
-        grid6.add(EWaste8Item);
+//        Bitmap EWaste1Im, EWaste2Im, EWaste3Im, EWaste4Im, EWaste5Im, EWaste6Im, EWaste7Im, EWaste8Im;
+//        EWaste1Im = BitmapFactory.decodeResource(getResources(), R.drawable.EWaste1);
+//        EWaste2Im = BitmapFactory.decodeResource(getResources(), R.drawable.EWaste2);
+//        EWaste3Im = BitmapFactory.decodeResource(getResources(), R.drawable.EWaste3);
+//        EWaste4Im = BitmapFactory.decodeResource(getResources(), R.drawable.EWaste4);
+//        EWaste5Im = BitmapFactory.decodeResource(getResources(), R.drawable.EWaste5);
+//        EWaste6Im = BitmapFactory.decodeResource(getResources(), R.drawable.EWaste6);
+//        EWaste7Im = BitmapFactory.decodeResource(getResources(), R.drawable.EWaste7);
+//        EWaste8Im = BitmapFactory.decodeResource(getResources(), R.drawable.EWaste8);
+//
+//        GridItem EWaste1Item = new GridItem();
+//        EWaste1Item.image = EWaste1Im;
+//        grid6.add(EWaste1Item);
+//
+//        GridItem EWaste2Item = new GridItem();
+//        EWaste2Item.image = EWaste2Im;
+//        grid6.add(EWaste2Item);
+//
+//        GridItem EWaste3Item = new GridItem();
+//        EWaste3Item.image = EWaste3Im;
+//        grid6.add(EWaste3Item);
+//
+//        GridItem EWaste4Item = new GridItem();
+//        EWaste4Item.image = EWaste4Im;
+//        grid6.add(EWaste4Item);
+//
+//        GridItem EWaste5Item = new GridItem();
+//        EWaste5Item.image = EWaste5Im;
+//        grid6.add(EWaste5Item);
+//
+//        GridItem EWaste6Item = new GridItem();
+//        EWaste6Item.image = EWaste6Im;
+//        grid6.add(EWaste6Item);
+//
+//        GridItem EWaste7Item = new GridItem();
+//        EWaste7Item.image = EWaste7Im;
+//        grid6.add(EWaste7Item);
+//
+//        GridItem EWaste8Item = new GridItem();
+//        EWaste8Item.image = EWaste8Im;
+//        grid6.add(EWaste8Item);
 
         GridItemAdapter gridItemAdapter = new GridItemAdapter(this, R.layout.grid_item, grid6);
         gridView6.setAdapter(gridItemAdapter);
--- app/src/main/java/com/example/guiltyrecycling/WriteActivity.java
+++ app/src/main/java/com/example/guiltyrecycling/WriteActivity.java
@@ -1,14 +1,63 @@
 package com.example.guiltyrecycling;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class WriteActivity extends AppCompatActivity {
+
+    ListView search_item;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.image_activity);
+
+            search_item = (ListView) findViewById(R.id.search_item);
+
+            ArrayList<String> arrayItem = new ArrayList<>();
+            arrayItem.addAll(Arrays.asList(getResources().getStringArray(R.array.my_items)));
+
+            adapter = new ArrayAdapter<String>(
+                    WriteActivity.this,
+                    android.R.layout.simple_list_item_1,
+                    arrayItem
+            );
+
+            search_item.setAdapter(adapter);
+
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.search_menu, menu);
+            MenuItem item = menu.findItem(R.id.search_item);
+            SearchView searchView = (SearchView) item.getActionView();
+
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String s) {
+                    return false;
+                }
+                @Override
+                public boolean onQueryTextChange(String s) {
+                    adapter.getFilter().filter(s);
+                    return false;
+                }
+
+            });
+
+            return super.onCreateOptionsMenu(menu);
+        }
     }
-}
